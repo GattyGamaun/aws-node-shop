2b2d23c40634b8740d20cf665a053d1ba1950e21
Siarhei_Kandratsiuk <siarhei_kandratsiuk@epam.com>
2023-10-23 19:54:59 +0200

    feat: add serialize methods

--- product-service/handlers/get-products-list.js
+++ product-service/handlers/get-products-list.js
@@ -1,14 +1,15 @@
-import { DynamoDBClient, BatchGetItemCommand } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient, BatchGetItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 
-const client = new DynamoDBClient({ region: "eu-west-3" });
+const client = new DynamoDBClient({ region: 'eu-west-3' });
 const docClient = DynamoDBDocumentClient.from(client);
 
 const getProductItems = async () => {
   const command = new ScanCommand({
-    ProjectionExpression: "#id, title, description, price",
-    ExpressionAttributeNames: { "#id": "id" },
-    TableName: process.env.TABLE_PRODUCTS,
+    ProjectionExpression: '#id, title, description, price',
+    ExpressionAttributeNames: { '#id': 'id' },
+    TableName: process.env.TABLE_PRODUCTS
   });
 
   const { Items } = await docClient.send(command);
@@ -20,9 +21,9 @@ const products = await getProductItems();
 const stockParams = {
   RequestItems: {
     stocks: {
-      Keys: products.map((product) => ({ product_id: { S: product.id } })),
-    },
-  },
+      Keys: products.map((product) => ({ product_id: marshall(product.id) }))
+    }
+  }
 };
 
 const batchStock = new BatchGetItemCommand(stockParams);
@@ -30,12 +31,12 @@ const stockItems = await docClient.send(batchStock);
 
 const response = products.map((product) => {
   const productInStock = stockItems.Responses.stocks.find(
-    (stock) => stock["product_id"]["S"],
+    (stock) => marshall(stock).product_id
   );
 
   return {
     ...product,
-    count: parseFloat(productInStock.count["N"]),
+    count: unmarshall(productInStock).count
   };
 });
 
