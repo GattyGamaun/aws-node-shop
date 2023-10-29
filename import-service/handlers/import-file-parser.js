c4db3c1a464f251bb28e7b12d381845102afc783
Siarhei_Kandratsiuk <siarhei_kandratsiuk@epam.com>
2023-10-29 18:46:46 +0100

    feat: add csv parser

--- import-service/handlers/import-file-parser.js
+++ import-service/handlers/import-file-parser.js
@@ -1,23 +1,27 @@
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import csv from 'csv-parser';
 
 const client = new S3Client({ region: 'eu-north-1' });
-
 export async function importFileParser(event) {
   const { Records } = event;
   const [record] = Records;
   const bucketName = record.s3.bucket.name;
   const key = record.s3.object.key;
-
-  const command = new GetObjectCommand({
+  const params = {
     Bucket: bucketName,
     Key: key
-  });
+  };
+  const command = new GetObjectCommand(params);
+  const results = [];
 
   try {
     const response = await client.send(command);
-    const stream = await response.Body.transformToString();
 
-    console.log('stream', stream);
+    response.Body.pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        console.log('results', results);
+      });
   } catch (err) {
     console.error(err);
   }
