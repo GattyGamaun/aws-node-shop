2ce29853bff5d54b701fcbdd6eccb882b0da3f0b
Siarhei_Kandratsiuk <siarhei_kandratsiuk@epam.com>
2023-11-12 17:24:59 +0100

    feat: develop 7.2

--- authorization-service/handler.js
+++ authorization-service/handler.js
@@ -1,37 +1,35 @@
 'use strict';
 require('dotenv').config();
 
+function generatePolicy(principalId, effect, arn) {
+  return {
+    principalId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: arn
+        }
+      ]
+    }
+  };
+}
+
 module.exports.basicAuthorizer = async (event) => {
   const {
+    routeArn,
     headers: { authorization }
   } = event;
-
-  if (!authorization) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({
-        message: 'Authorization header is not provided'
-      })
-    };
-  }
 
   const [, token] = authorization.split(' ');
   const decoded = Buffer.from(token, 'base64').toString('utf8');
   const [username, password] = decoded.split(':');
 
   if (username === process.env.USER_NAME && password === process.env.USER_PSW) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'You have been granted access'
-      })
-    };
+    return generatePolicy(token, 'Allow', routeArn);
   } else {
-    return {
-      statusCode: 403,
-      body: JSON.stringify({
-        message: 'Access is denied. Unauthorized'
-      })
-    };
+    return generatePolicy(token, 'Deny', routeArn);
   }
 };
